6390b09c674b1b4543b1a7bd7534a91d1f26f8a8
TheRealToky <tokydisaine.andrianarison@gmail.com>
2026-09-13 21:33:03 +0200

    3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,7 +3,8 @@
 
 def matrix_transpose(matrix):
     """Does stuff"""
-    matrix_t = []
+    matrix_t = [[0 for _ in range(len(matrix))]
+                for _ in range(len(matrix[0]))]
 
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
