0f6c17220afdf940a4aef9327f51ed3996aae4ea
TheRealToky <tokydisaine.andrianarison@gmail.com>
2026-09-13 21:57:11 +0200

    5

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Calculate the shape of an Ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of an Ndarray
+    """
+    return tuple(matrix.shape)
---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Does stuff
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Does stuff
+    """
+    return arr1 + arr2
