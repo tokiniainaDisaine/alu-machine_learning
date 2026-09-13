8ca63176b1be34e69a56b840440a484e5ae8991f
TheRealToky <tokydisaine.andrianarison@gmail.com>
2026-09-13 21:51:43 +0200

    5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -2,28 +2,11 @@
 """Stuff"""
 
 
-def matrix_shape(matrix):
-    """
-    docstring for matrix_shape
-    dsfghnsdfghjgfd
-    """
-    shape = [len(matrix)]
-
-    if isinstance(matrix[0], list):
-        shape.extend(matrix_shape(matrix[0]))
-
-    return shape
-
-
 def add_matrices2D(mat1, mat2):
-    """Stuff"""
-    mat_final = [[0 for _ in range(len(mat1))]
-                for _ in range(len(mat1[0]))]
-
-    if matrix_shape(mat1) != matrix_shape(mat2):
+    """
+    Does stuff
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    else:
-        for i in range(len(mat1)):
-            for j in range(len(mat1[i])):
-                mat_final[i][j] = mat1[i][j] + mat2[i][j]
-        return mat_final
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
